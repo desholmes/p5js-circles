378ca027153b7d1dc40b411db32ba0f208413d7f
desholmes <desholmes@gmail.com>
2022-11-24 09:42:53 +0000

    Updated docs, hidden home link by default

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import p5 from "p5";
 import sketch from "./sketch";
 
 new p5(sketch);
+
+const init = () => {
+  if (window.location.hostname == "dholmes.co.uk") {
+    document.querySelector(".home-link").classList.remove("hide");
+  }
+};
+
+if (
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  init();
+} else {
+  document.addEventListener("DOMContentLoaded", init);
+}
